bb6aefdb597e01e909e4e91e0e02554a3883e2a0
Brian Dunn <brian@theophil.us>
2018-12-21 15:21:00 -0600

    pretty toggling

--- src/Main.elm
+++ src/Main.elm
@@ -1,18 +1,14 @@
-module Main exposing (..)
+module Main exposing (Action(..), Model, PathCommand(..), colors, commandToString, interval, main, noteWave, onChange, pathDefinition, update, view, wave)
 
-import Json.Decode as Json
 import Browser
-import Svg exposing (path, svg)
 import Html exposing (div, input)
 import Html.Attributes exposing (style, type_)
-import Svg.Attributes exposing (fill, width, height, viewBox, d, stroke, strokeWidth)
-import String
-import Html.Events exposing (onClick, on)
+import Html.Events exposing (on, onClick)
+import Json.Decode as Json
 import Set exposing (Set)
-
-
-type alias Point =
-    { x : Float, y : Float }
+import String
+import Svg exposing (path, svg)
+import Svg.Attributes exposing (d, fill, height, stroke, strokeWidth, viewBox, width)
 
 
 type alias Model =
@@ -34,15 +30,16 @@ update action model =
         ToggleKey i ->
             if Set.member i model.notes then
                 { model | notes = Set.remove i model.notes }
+
             else
                 { model | notes = Set.insert i model.notes }
 
         Zoom stringValue ->
             let
                 zoom =
-                    (stringValue |> String.toFloat >> (Maybe.withDefault 0)) / 100.0
+                    (stringValue |> String.toFloat >> Maybe.withDefault 0) / 100.0
             in
-                { model | zoom = zoom }
+            { model | zoom = zoom }
 
 
 type PathCommand
@@ -67,33 +64,21 @@ pathDefinition commands =
 
 wave : Int -> Float -> List PathCommand
 wave samples cycles =
-    (M 0 0)
+    M 0 0
         :: (List.range 0 samples
                 |> List.map toFloat
                 |> List.map
                     (\i ->
-                        (L
-                            (2 * (i / (toFloat samples)))
-                            (sin (cycles * 2 * i * pi / (toFloat samples)))
-                        )
+                        L
+                            (2 * (i / toFloat samples))
+                            (sin (cycles * 2 * i * pi / toFloat samples))
                     )
            )
 
 
-parsePoint : Json.Decoder Point
-parsePoint =
-    Json.map2 Point
-        (Json.field "clientX" Json.float)
-        (Json.field "clientY" Json.float)
-
-
-onMouseOver point =
-    Html.Events.on "mouseover" parsePoint
-
-
 interval : Int -> Float
 interval n =
-    Debug.log "freq" (2 ^ ((toFloat n) / 12.0))
+    2 ^ (toFloat n / 12.0)
 
 
 colors =
@@ -104,39 +89,47 @@ noteWave note color zoom =
     path
         [ fill "none"
         , stroke color
-        , strokeWidth "0.01"
-        , d (pathDefinition (wave 500 (zoom * (interval note))))
+        , strokeWidth "0.03"
+        , d (pathDefinition (wave 500 (zoom * interval note)))
         ]
         []
-
-
-
--- (List.map2 noteWave (Set.toList model) colors)
 
 
 onChange tagger =
     on "input" (Json.map tagger Html.Events.targetValue)
 
 
+keyboard notes =
+    div
+        [ style "flex" "1"
+        , style "display" "flex"
+        , style "flex-direction" "column"
+        ]
+        (colors
+            |> List.indexedMap
+                (\i color ->
+                    div
+                        [ style "background-color" color
+                        , style "flex" "1"
+                        , style "margin" "2px"
+                        , style "box-shadow"
+                            (if Set.member i notes then
+                                "unset"
+
+                             else
+                                "0px 0px 10vh inset white"
+                            )
+                        , onClick (ToggleKey i)
+                        ]
+                        []
+                )
+        )
+
+
 view : Model -> Html.Html Action
 view model =
     div [ style "display" "flex" ]
-        [ div
-            [ style "flex" "1"
-            , style "display" "flex"
-            , style "flex-direction" "column"
-            ]
-            (colors
-                |> List.indexedMap
-                    (\i color ->
-                        div
-                            [ style "background-color" color
-                            , style "flex" "1"
-                            , onClick (ToggleKey i)
-                            ]
-                            []
-                    )
-            )
+        [ keyboard model.notes
         , div
             [ style "flex" "4" ]
             [ svg [ width "500", height "500", viewBox "0 -1 2 2" ]
@@ -144,6 +137,7 @@ view model =
                     (\note color ->
                         if Set.member note model.notes then
                             noteWave note color (model.zoom * 5 + 1)
+
                         else
                             path [] []
                     )
